165f8ebb608d0fb513facc98f6c29ab5e0ba048a
Ely Merenstein <elysamuel16@gmail.com>
2020-07-07 14:50:38 -0600

    fix bug in previous commit and add comments, clean up overall. also add gitignore and a note about metadata

--- data_processor.py
+++ data_processor.py
@@ -1,37 +1,44 @@
+import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import stopwords
 STOPWORDS = stopwords.words('english')
 import re
 
-def process_file(filename):
+def process_file(filename, stop_after_rows=500):
     data = pd.read_csv(filename)
     data = data.values
-    posts = data[:,1]
+    posts = data[:stop_after_rows,1]
     vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(posts) # counts in a 2D matrix
     preprocessor = vectorizer.build_preprocessor()
+    X = np.empty(0)
+    print("Preprocessing progress (by rows of original data):")
     for i in range(posts.shape[0]):
-        comments = preprocessor(posts[i])
-        comment_list = comments.split('\n')
-        for j in range(len(comment_list[:-1])):
-            comment = comment_list[i]
-            comment_list[j] = comment[(comment.index('.') + 1):].strip()
-        if i==36: print(comment_list)
+        if i % 100 == 0: print("%.0f%%" % (i*100/posts.shape[0]))
+        row_posts_string = preprocessor(posts[i]) # preprocess the posts in this row (including making them lowercase)
+        row_posts_list = row_posts_string.split('\n')[:-1] # split up all the posts in a given row (and ignore the last one, since it's always empty)
+        for post in row_posts_list:
+            post = post[(post.index('.') + 1):].strip() # remove the prepended index (e.g. "2.") and tab characters
+            # TODO: more preprocessing
+                # remove stopwords
+                # remove unique words (maybe this isn't the right place to do that? idk)
+                # get rid of URLs
+                # emojis -- not sure
+                # address misspelling of significant words
+            X = np.append(X, post) # add it to our 1D numpy array of all posts
     
+    counts = vectorizer.fit_transform(X) # counts in a 2D matrix
+    print(counts[0]) # just for playing around/testing
+    # etc. 
     
 
     # data = re.sub(r'http\S+','',data)
     
-    # Preprocessing thoughts: 
-    # lowercase
-    # get rid of URLs
-    # emojis -- not sure
-    # address misspelling of significant words
+    
 
 def main():
     gab_data = process_file('data/gab.csv')
-    #gab_data = process_file('data/reddit.csv')
+    #reddit_data = process_file('data/reddit.csv')
 
 if __name__ == "__main__":
     main()
