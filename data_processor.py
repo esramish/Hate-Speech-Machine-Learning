2256cf5be65019be21ae61bdf54f4d2c799c341a
Ely Merenstein <elysamuel16@gmail.com>
2020-07-09 13:08:53 -0600

    remove words unique to a single post. also remove some punctuation, fix stopwords apostrophe bug, update preprocessor method so that it returns the list of feature names as well

--- data_processor.py
+++ data_processor.py
@@ -27,8 +27,10 @@ class Processor:
             j = 1
             for post in row_posts_list:
                 post = post[(post.index('.') + 1):].strip() # remove the prepended index (e.g. "2.") and tab characters
+                post = re.sub(r'\.|,|;|:|\?|!|\(|\)|\'|"|\u201C|\u201D', '', post) # remove certain punctuation
                 
                 # remove stopwords 
+                post = re.sub(r'\u2018|\u2019', "'", post) # replace smart (curly) apostrophes with ASCII apostrophes, since that's what nltk uses
                 post_words = post.split()
                 post_words = list(filter(lambda word: word not in STOPWORDS, post_words))
                 post = " ".join(post_words)
@@ -36,9 +38,8 @@ class Processor:
                 # get rid of URLs
                 post = re.sub( r'http\S+', '', post )
                 
-                # TODO: more preprocessing
-                    # remove unique words (maybe this isn't the right place to do that? idk)
-                    # emojis -- not sure
+                # TODO: potential further preprocessing ideas:
+                    # emojis -- not sure, might want to leave them (although we've already gotten rid of some punctuation and therefore punctuation-emojis, currently)
                     # address misspelling of significant words
                 list_of_all_posts = np.append(list_of_all_posts, post) # add it to our 1D numpy array of all posts
                 
@@ -60,16 +61,24 @@ class Processor:
         print("100%")
 
         counts = vectorizer.fit_transform(list_of_all_posts) # counts in a 2D matrix
-        counts_np = np.array(counts.toarray())
+        counts_np = np.array(counts.toarray()) # convert to normal numpy format
 
-        return counts_np, Y
+        feature_names = vectorizer.get_feature_names() # the 1D python list of features (i.e. words) that correspond to the columns of counts_np
+        feature_names_np = np.array(feature_names) # convert to numpy
+
+        # remove unique features/columns (i.e. words that appear only in one post throughout the corpus)
+        non_unique_indeces = np.nonzero(np.count_nonzero(counts_np,axis=0)>1)[0] # the column indeces of the features that appear in more than one document throughout the corpus
+        non_unique_counts_np = counts_np[:,non_unique_indeces] # select only the columns at those indeces
+        non_unique_feature_names_np = feature_names_np[non_unique_indeces] # select only the feature names at those indeces
+
+        return non_unique_counts_np, non_unique_feature_names_np, Y
 
         # print(np.array(vectorizer.get_feature_names())[np.nonzero(counts[0])[1]]) # good for seeing the word counts of a single post
     
 
 def main():
     p = Processor()
-    gab_X, gab_Y = p.process_files('data/gab.csv', stop_after_rows=50)
+    gab_X, gab_feature_names, gab_Y = p.process_files('data/gab.csv', stop_after_rows=50)
     # total_counts = gab_X.sum(0)
 
 if __name__ == "__main__":
