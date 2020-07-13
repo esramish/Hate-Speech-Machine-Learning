0d992861c4a7c15906eaf22ddd3efa4b16b4f327
Stephen Sigman <stephen.sigman13@gmail.com>
2020-07-13 10:18:00 -0600

    Build basic RNN for text generation

--- text_generation.py
+++ text_generation.py
@@ -10,10 +10,12 @@ import numpy as np
 def main():
     p = Processor()
     gab_X, gab_feature_names, responses, gab_Y = p.process_files('data/gab.csv', stop_after_rows=50)
+    responses = np.array(responses)
+    responses_str = responses[:,0]
     # total_counts = gab_X.sum(0)
     textgen = textgenrnn()
-    textgen.train_on_texts(responses,num_epochs = 10)
-    textgen.generate_samples(3)
+    textgen.train_on_texts(responses_str,num_epochs = 10)
+    textgen.generate(3)
 
 if __name__ == "__main__":
     main()
